d385233b663d76b0ff1627ce30328d435fb4922f
TuanVu <tuanvu@MacBook-Pro-5.local>
2025-03-03 21:22:34 +0700

    Fix

--- app/workers/create_report_worker.rb
+++ app/workers/create_report_worker.rb
@@ -4,22 +4,24 @@ class CreateReportWorker
   def perform date_execute = Date.current
     reports = []
 
-    (date_execute - 1.weeks..date_execute).each do |date|
+    (date_execute - 1.weeks..date_execute).each.with_index do |date, i|
       User.all.each do |user|
         unless UserDailyReport.exists?(user_id: user.id, start_date: date)
           reports << UserDailyReport.new(user_id: user.id, content: {}, start_date: date)
         end
 
-        unless UserWeeklyReport.exists?(user_id: user.id, start_date: date.beginning_of_week)
-          reports << UserWeeklyReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_week)
-        end
+        if i.zero?
+          unless UserWeeklyReport.exists?(user_id: user.id, start_date: date.beginning_of_week)
+            reports << UserWeeklyReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_week)
+          end
 
-        unless UserMonthlyReport.exists?(user_id: user.id, start_date: date.beginning_of_month)
-          reports << UserMonthlyReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_month)
-        end
+          unless UserMonthlyReport.exists?(user_id: user.id, start_date: date.beginning_of_month)
+            reports << UserMonthlyReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_month)
+          end
 
-        unless UserQuarterReport.exists?(user_id: user.id, start_date: date.beginning_of_quarter)
-          reports << UserQuarterReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_quarter)
+          unless UserQuarterReport.exists?(user_id: user.id, start_date: date.beginning_of_quarter)
+            reports << UserQuarterReport.new(user_id: user.id, content: {}, start_date: date.beginning_of_quarter)
+          end
         end
       end
 
@@ -27,16 +29,18 @@ class CreateReportWorker
         reports << DailyReport.new(content: {}, start_date: date)
       end
 
-      unless WeeklyReport.exists?(start_date: date.beginning_of_week)
-        reports << WeeklyReport.new(content: {}, start_date: date.beginning_of_week)
-      end
+      if i.zero?
+        unless WeeklyReport.exists?(start_date: date.beginning_of_week)
+          reports << WeeklyReport.new(content: {}, start_date: date.beginning_of_week)
+        end
 
-      unless MonthlyReport.exists?(start_date: date.beginning_of_month)
-        reports << MonthlyReport.new(content: {}, start_date: date.beginning_of_month)
-      end
+        unless MonthlyReport.exists?(start_date: date.beginning_of_month)
+          reports << MonthlyReport.new(content: {}, start_date: date.beginning_of_month)
+        end
 
-      unless QuarterReport.exists?(start_date: date.beginning_of_quarter)
-        reports << QuarterReport.new(content: {}, start_date: date.beginning_of_quarter)
+        unless QuarterReport.exists?(start_date: date.beginning_of_quarter)
+          reports << QuarterReport.new(content: {}, start_date: date.beginning_of_quarter)
+        end
       end
     end
 
