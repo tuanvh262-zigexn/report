e1e9a7c4b161bbd55660b67aa697d61bfb3d3368
TuanVu <tuanvu@MacBook-Pro-5.local>
2025-06-16 11:37:07 +0700

    Fix

--- app/workers/update_redmine_issue_worker.rb
+++ app/workers/update_redmine_issue_worker.rb
@@ -2,9 +2,9 @@ class UpdateRedmineIssueWorker
   include Sidekiq::Worker
 
   def perform redmine_issue_id, payload = nil
-    task = SubTask.find_by(issue_id: redmine_issue_id) || Story.find_by(issue_id: redmine_issue_id)
+    task = SubTask.find_by(issue_id: redmine_issue_id)
 
-    return if task.nil? || task.closed?
+    return if task&.closed?
 
     Redmine::UpdateIssue.new(redmine_issue_id, payload).execute
   end
