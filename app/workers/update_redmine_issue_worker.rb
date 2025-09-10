5841ba26f26ffd68fea622d679e7cae4db4bff37
TuanVu <tuanvu@MacBook-Pro-5.local>
2025-09-10 20:25:02 +0700

    Fix

--- app/workers/update_redmine_issue_worker.rb
+++ app/workers/update_redmine_issue_worker.rb
@@ -4,6 +4,8 @@ class UpdateRedmineIssueWorker
   def perform redmine_issue_id, payload = nil
     task = SubTask.without_parent_tasks.find_by(issue_id: redmine_issue_id)
 
+    return if task&.closed? && JSON.parse(payload)["subject"].nil?
+
     Redmine::UpdateIssue.new(redmine_issue_id, payload).execute
   end
 end
